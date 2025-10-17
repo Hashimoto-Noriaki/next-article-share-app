9273ebd3689e8495893a2ae17829b857c96b28ae
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-10-18 02:42:32 +0900

    buttonがカラーリングされるかも確認

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/shared/components/ui/button"
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-        <Button>
+        <Button className="bg-sky-500 h-[80px]">
         プロジェクトスタート
         </Button>
     </div>
