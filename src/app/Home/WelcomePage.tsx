64c4eabaf1222b2513e842a424aa1241e60283a8
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-10-20 03:21:06 +0900

    ヘッターに表示

--- src/app/Home/WelcomePage.tsx
+++ src/app/Home/WelcomePage.tsx
@@ -1,5 +1,21 @@
 export default function WelcomePage(){
     return (
-        <div>WelcomePage</div>
+        <div>
+            <header>
+                <div className="flex justify-between">
+                    <div>
+                        <h1>テックブログ共有アプリ</h1>
+                        <p>エンジニア同士で有益な記事を共有しよう</p>
+                    </div>
+                    <nav>
+                        <ul className="flex gap-5">
+                            <li>利用説明</li>
+                            <li>新規登録</li>
+                            <li>ログイン</li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        </div>
     )
 }
