d8c05b2b050cd5be71015aa022247edae60f9e89
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-10-20 03:13:47 +0900

    WelcomePageをインポート

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import WelcomePage from './Home/WelcomePage'
+
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl text-sky-500 font-bold">プロジェクトスタート</h1>
-    </div>
-  );
+  return <WelcomePage/>
 }
