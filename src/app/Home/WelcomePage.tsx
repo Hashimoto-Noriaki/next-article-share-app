334c64de63829452aff25c2cf23a8d5e60bdf18b
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-10-20 03:31:56 +0900

    ヘッターにcss適用

--- src/app/Home/WelcomePage.tsx
+++ src/app/Home/WelcomePage.tsx
@@ -1,14 +1,14 @@
 export default function WelcomePage(){
     return (
         <div>
-            <header>
+            <header className="bg-gradient-to-br from-cyan-500 to-cyan-600 h-[15vh] p-3">
                 <div className="flex justify-between">
-                    <div>
-                        <h1>テックブログ共有アプリ</h1>
-                        <p>エンジニア同士で有益な記事を共有しよう</p>
+                    <div className="font-bold">
+                        <h1 className="text-3xl text-amber-500 mb-5">テックブログ共有アプリ</h1>
+                        <p className="text-white text-2xl">エンジニア同士で有益な記事を共有しよう</p>
                     </div>
                     <nav>
-                        <ul className="flex gap-5">
+                        <ul className="flex gap-5 text-white font-bold p-10">
                             <li>利用説明</li>
                             <li>新規登録</li>
                             <li>ログイン</li>
