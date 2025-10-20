ceae479892b720d956241db9c280b1dc48ac873d
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-10-20 22:29:01 +0900

    アイコンを配置

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,8 +1,12 @@
+import { FaLaptopCode } from 'react-icons/fa';
+
 export default function LoginPage() {
     return (
         <div className="flex items-center justify-center p-20 max-h-screen">
             <div className="bg-gradient-to-r from-cyan-400 to-cyan-500 px-16 py-24 text-center w-full  max-w-md rounded-md">
-                <h1 className="text-2xl text-white font-bold">テックブログ共有アプリ</h1>
+                <h1 className="text-2xl flex items-center text-white font-bold">
+                    <FaLaptopCode/>テックブログ共有アプリ
+                </h1>
                 <h2 className="text-xl text-white font-bold mt-3">ログイン</h2>
                 <form className="flex flex-col gap-5 text-left">
                     <div>
