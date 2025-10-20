8b75a93c6a9c2993f2181312b345eebd88b57b92
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-10-20 22:13:03 +0900

    グラデーションの変更

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,15 +1,15 @@
 export default function LoginPage() {
     return (
         <div className="flex items-center justify-center p-10 max-h-screen">
-            <div className="bg-gradient-to-r from-cyan-300 to-cyan-400 px-16 py-24 text-center w-full  max-w-md rounded-md">
-                <h1 className="text-2xl text-white font-bold">テックブログ共有アプリ</h1>
-                <h2 className="text-xl text-white font-bold mt-3">ログイン</h2>
+            <div className="bg-gradient-to-r from-cyan-400 to-cyan-600 px-16 py-24 text-center w-full  max-w-md rounded-md">
+                <h1 className="text-3xl text-white font-bold">テックブログ共有アプリ</h1>
+                <h2 className="text-2xl text-white font-bold mt-3">ログイン</h2>
                 <form className="flex flex-col gap-5 text-left">
                     <div>
                         <p className="font-bold mb-3">メールアドレス</p>
                         <input
                             name="email"
-                            className="w-full p-3 border border-cyan-800 bg-white rounded-lg focus:outline-none qfocus:ring-cyan-800"
+                            className="w-full p-3 border border-cyan-900 bg-white rounded-lg focus:outline-none focus:ring-cyan-900"
                             placeholder="メールアドレス"
                         />
                     </div>
