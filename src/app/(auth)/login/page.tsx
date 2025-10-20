17a150b73a332495ef32699aeb740dcbf91381c9
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-10-20 19:29:04 +0900

    ログイン画面の雛形

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,28 @@
 export default function LoginPage() {
-    return <div>ログインページ</div>
+    return (
+        <div className="flex items-center justify-center p-20 max-h-screen">
+            <div className="bg-gradient-to-r from-cyan-500 to-cyan-600 px-16 py-24 text-center w-full  max-w-md rounded-md">
+                <h1 className="text-2xl text-white font-bold">テックブログ共有アプリ</h1>
+                <h2 className="text-xl text-white font-bold mt-3">ログイン</h2>
+                <form>
+                    <div>
+                        <p className="">メールアドレス</p>
+                        <input
+                            name="email"
+                            className=""
+                            placeholder="メールアドレス"
+                        />
+                    </div>
+                    <div>
+                        <p className="">パスワード</p>
+                        <input
+                            name="password"
+                            className=""
+                            placeholder="パスワード"
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
 }
