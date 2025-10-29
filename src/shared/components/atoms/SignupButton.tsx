51672105f377c53796b8dfdd05121f849185cc1a
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-10-29 19:13:03 +0900

    atomsのボタンコンポーネントのrounded-lgをrounded-fullにする

--- src/shared/components/atoms/SignupButton.tsx
+++ src/shared/components/atoms/SignupButton.tsx
@@ -9,7 +9,7 @@ type PropsType = {
 export default function SignupButton({ children, ...props }: PropsType) {
   return (
     <button
-      className="bg-amber-500 text-white  font-bold rounded-lg p-5 mt-5 hover:bg-amber-400"
+      className="bg-amber-500 text-white  font-bold rounded-full p-5 mt-5 hover:bg-amber-400"
       {...props}
     >
       {children}
