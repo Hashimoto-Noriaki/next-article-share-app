ab0286e37f98f8cc383c9244f7bb3fd6f86fbeda
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2025-12-05 00:29:58 +0900

    記事の新規投稿ページのヘッターをコンポーネント化

--- src/app/(articles)/articles/new/page.tsx
+++ src/app/(articles)/articles/new/page.tsx
@@ -5,6 +5,7 @@ import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import remarkBreaks from 'remark-breaks';
 import 'github-markdown-css/github-markdown.css';
+import { ArticleHeader } from '@/features/articles/components/ArticleHeader'
 
 export default function NewArticlePage() {
   const [title, setTitle] = useState('');
@@ -16,15 +17,8 @@ export default function NewArticlePage() {
 
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
-      <header className="bg-gradient-to-r from-cyan-500 to-cyan-600 border-slate-200 px-5 py-4 flex justify-between shadow-sm">
-        <h1 className="text-2xl font-bold text-white">記事を投稿</h1>
-        <button
-          className="bg-emerald-500 hover:bg-emerald-400 text-white font-semibold px-6 py-2.5 rounded-md shadow transition"
-          onClick={[]}
-        >
-          公開設定へ
-        </button>
-      </header>
+      <ArticleHeader onPublish={()=> {}} />
+        {/* TODO: 公開設定の実装は後ほど追加する */}
       <main className="flex-grow container mx-auto px-5 py-5 pt-1 w-[100%]">
         <input
           value={title}
