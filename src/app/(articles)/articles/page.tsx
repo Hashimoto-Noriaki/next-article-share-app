cd3f0369888ea4828e1ecd5c341b6c789d2b04df
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2025-12-06 01:41:58 +0900

    tailwindのスペルミス修正

--- src/app/(articles)/articles/page.tsx
+++ src/app/(articles)/articles/page.tsx
@@ -8,7 +8,7 @@ export default function ArticleList() {
     <div className="flex flex-col min-h-screen">
       <ArticleListHeader />
       <main className="grow flex flex-col items-center justify-start mt-5">
-        <div className="bg-leaner-to-r from-rose-300 to-cyan-600 px-8 py-12 font-bold text-white w-full max-w-5xl text-center rounded-lg shadow-lg">
+        <div className="bg-linear-to-r from-rose-300 to-cyan-600 px-8 py-12 font-bold text-white w-full max-w-5xl text-center rounded-lg shadow-lg">
           <h1 className="text-5xl">テックブログ共有アプリ</h1>
           <p className="text-3xl mt-5">
             エンジニア同士で有益な記事を共有しよう
