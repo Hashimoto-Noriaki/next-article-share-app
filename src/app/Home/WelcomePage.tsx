30b3d43903ca32d896e4bdb627fbc3984740bf8c
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2025-12-07 04:26:09 +0900

    WelcomePageのヘッターをコンポーネント化

--- src/app/Home/WelcomePage.tsx
+++ src/app/Home/WelcomePage.tsx
@@ -3,41 +3,12 @@ import { FaLaptopCode } from 'react-icons/fa';
 import Footer from '../../shared/components/footer';
 import LoginButton from '../../shared/components/atoms/LoginButton';
 import SignupLeadButton from '../../shared/components/atoms/SignupLeadButton';
+import WelcomeHeader from '../../features/home/WelcomeHeader';
 
 export default function WelcomePage() {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="bg-gradient-to-br from-cyan-500 to-cyan-600 h-[15vh] p-3">
-        <div className="flex justify-between">
-          <div className="font-bold">
-            <h1 className="text-3xl text-amber-500 mb-5">
-              テックブログ共有アプリ
-            </h1>
-            <p className="text-white text-2xl">
-              ×エンジニア同士で有益な記事を共有しよう
-            </p>
-          </div>
-          <nav>
-            <ul className="flex gap-5 text-white text-lg font-bold p-10">
-              <li>
-                <Link href="/guide" className="hover:text-amber-400">
-                  利用説明
-                </Link>
-              </li>
-              <li>
-                <Link href="/signup" className="hover:text-amber-400">
-                  新規登録
-                </Link>
-              </li>
-              <li>
-                <Link href="/login" className="hover:text-amber-400">
-                  ログイン
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
+      <WelcomeHeader/>
       <main className="flex-grow flex flex-col items-center justify-center">
         <div className="bg-gradient-to-r from-rose-300 to-cyan-600 px-16 py-24 font-bold text-white w-full max-w-5xl text-center rounded-lg shadow-lg">
           <h1 className="text-5xl flex items-center justify-center gap-3">
