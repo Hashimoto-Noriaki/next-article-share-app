95ca93170b99d8f2ee039cd38a43070f2a10d695
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2025-12-09 00:11:09 +0900

    ログインページのボタン修正

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -78,7 +78,7 @@ export default function LoginPage() {
               required
             />
           </div>
-          <Button>
+          <Button variant="primary">
             ログイン
           </Button>
           <Link
