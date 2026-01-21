69f6eaed63152f07cc3bfaf58085e914eea24fbb
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-01-21 22:43:31 +0900

    DeleteButtonからArticleDeleteButtonに変更して設計の修正

--- src/features/articles/components/index.tsx
+++ src/features/articles/components/index.tsx
@@ -1,5 +1,4 @@
 export { NewArticleHeader } from './NewArticleHeader';
 export { MarkdownEditor } from './MarkdownEditor';
-export { DeleteButton } from './DeleteButton';
 export { UserDropdown } from './UserDropdown';
 export { LikeButton } from './LikeButton';
