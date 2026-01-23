5089ae721d25a0c1d1dc6fcbfa3b7947907256cf
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-01-24 03:27:38 +0900

    NavigationHeader作成

---
+++ src/shared/components/molecules/NavigationHeader.tsx/NavigationHeader.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import Link from 'next/link';
+import { UserDropdown } from '../../../../features/articles/components/UserDropdown';
+
+type Props = {
+  userId: string;
+  userName: string;
+};
+
+export function NavigationHeader({ userId, userName }: Props) {
+  return (
+    <nav className="flex  items-center gap-8 text-white font-bold">
+      <Link href="/articles/new" className="hover:text-amber-400">
+        新規投稿
+      </Link>
+      <UserDropdown userId={userId} userName={userName} className="hover:text-amber-400"/>
+    </nav>
+  );
+}
