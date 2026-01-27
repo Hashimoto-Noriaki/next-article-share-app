70ff420945707c369db52349a917861ea5eb16ff
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-01-28 04:22:32 +0900

    StoryBookにaddonsを導入

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,9 @@ import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [],
+  addons: [
+    '@storybook/addon-a11y',  // ← これを追加
+  ],
   framework: {
     name: '@storybook/nextjs',
     options: {},
