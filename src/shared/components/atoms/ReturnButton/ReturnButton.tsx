4f22fb0e547419d8a010e210ca105fd164209afc
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-01-27 21:28:38 +0900

    fix

--- src/shared/components/atoms/ReturnButton/ReturnButton.tsx
+++ src/shared/components/atoms/ReturnButton/ReturnButton.tsx
@@ -7,12 +7,12 @@ type PropsType = {
   children: ReactNode;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-export default function SignupButton({ children, ...props }: PropsType) {
+export function ReturnButton({ children, ...props }: PropsType) {
   const router = useRouter();
   return (
     <button
       onClick={() => router.push('/')}
-      className="w-[20%] bg-cyan-600 text-white  font-semibold rounded-full p-5 mt-5 hover:bg-cyan-500"
+      className="w-[20%] bg-cyan-600 text-white font-semibold rounded-full p-5 mt-5 hover:bg-cyan-500"
       {...props}
     >
       {children}
