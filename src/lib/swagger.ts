1d6c06d80a4eda3f1ff96b5f8feb3b799b6d3280
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-01-27 19:09:22 +0900

    format

--- src/lib/swagger.ts
+++ src/lib/swagger.ts
@@ -2,9 +2,9 @@ import yaml from 'js-yaml';
 import fs from 'fs';
 import path from 'path';
 
-export const getApiDocs = () => {
+export const getApiDocs = (): Record<string, unknown> => {
   const filePath = path.join(process.cwd(), '.docs', 'api', 'openapi.yaml');
   const fileContents = fs.readFileSync(filePath, 'utf8');
-  const spec = yaml.load(fileContents);
+  const spec = yaml.load(fileContents) as Record<string, unknown>;
   return spec;
 };
