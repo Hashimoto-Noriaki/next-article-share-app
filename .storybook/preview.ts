450506a04267f93f23af96d51bbb71c742de05bc
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-01-27 21:47:53 +0900

    format

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,9 +1,9 @@
 import type { Preview } from '@storybook/nextjs';
-import '../src/app/globals.css'; 
+import '../src/app/globals.css';
 
 const preview: Preview = {
   parameters: {
-    layout: 'centered',//中央に配置
+    layout: 'centered', //中央に配置
     controls: {
       matchers: {
         color: /(background|color)$/i,
