d6b7d8ddcc74a246913cedf1dd35afe83e82ba06
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-01-28 01:51:24 +0900

    format

--- src/shared/components/atoms/ArticleReturnButton/ArticleReturnButton.stories.tsx
+++ src/shared/components/atoms/ArticleReturnButton/ArticleReturnButton.stories.tsx
@@ -1,4 +1,4 @@
-import type { meta,StoryObj } from '/@storybook/nextjs';
+import type { meta, StoryObj } from '/@storybook/nextjs';
 import { ArticleReturnButton } from './ArticleReturnButton';
 
 const meta: Meta<typeof ArticleReturnButton> = {
@@ -13,12 +13,11 @@ const meta: Meta<typeof ArticleReturnButton> = {
   ],
 };
 
-
 export default meta;
 type Story = StoryObj<typeof ArticleReturnButton>;
 
 export const Default: Story = {
-    args: {
+  args: {
     children: '戻る(記事一覧など)',
   },
-} 
+};
