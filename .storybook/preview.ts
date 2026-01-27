d357176ec41dcfb459643ad9378395974debe179
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-01-27 21:46:34 +0900

    コンポーネントを中央配置できるようにする

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,9 @@
 import type { Preview } from '@storybook/nextjs';
+import '../src/app/globals.css'; 
 
 const preview: Preview = {
   parameters: {
+    layout: 'centered',//中央に配置
     controls: {
       matchers: {
         color: /(background|color)$/i,
