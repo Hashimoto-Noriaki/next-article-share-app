360c191e23b1b1104db9a71e029e65db564ef650
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-01-29 21:15:19 +0900

    Stockのコメントを定義

---
+++ src/types/stock.ts
@@ -0,0 +1,19 @@
+export type Stock = {
+    id: string;
+    userId: string;
+    articleId: string;
+    createdAt: string;
+};
+
+export type StockWithArticle = {
+    article: {
+        id: string;
+        title: string;
+        tags: string[];
+        likeCount: number;
+        createdAt: string;
+        author: {
+            name: string | null;
+        };
+    };
+};
