6cd979d087099a08f460164e09f05c4875a36332
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-02-26 02:26:33 +0900

    format

--- src/external/repository/user/user.repository.ts
+++ src/external/repository/user/user.repository.ts
@@ -1,14 +1,14 @@
 import { prisma } from '@/lib/prisma';
 
 export const userRepository = {
-    findById: async (userId: string) => {
-        return prisma.user.findUnique({
-            where: { id: userId },
-            include: {
-                articles: {
-                    orderBy: { createdAt: 'desc' },
-                },
-            },
-        });
-    },
+  findById: async (userId: string) => {
+    return prisma.user.findUnique({
+      where: { id: userId },
+      include: {
+        articles: {
+          orderBy: { createdAt: 'desc' },
+        },
+      },
+    });
+  },
 };
