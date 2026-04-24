8f148c32f90d207266cb8f57e0333ca4d1d7f019
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-04-24 10:27:16 +0900

    .docsをdocsに変更した実装の残り

--- src/lib/swagger.ts
+++ src/lib/swagger.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 import path from 'path';
 
 export const getApiDocs = (): Record<string, unknown> => {
-  const filePath = path.join(process.cwd(), '.docs', 'api', 'openapi.yaml');
+  const filePath = path.join(process.cwd(), 'docs', 'api', 'openapi.yaml');
   const fileContents = fs.readFileSync(filePath, 'utf8');
   const spec = yaml.load(fileContents) as Record<string, unknown>;
   return spec;
